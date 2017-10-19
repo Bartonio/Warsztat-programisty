a82cb48098178d1bf28e868d79aa661d8738a258
Bartonio <bartonioqli@gmail.com>
2017-10-19 09:15:39 +0200

    Add files via upload

---
+++ Pole trojkata.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+ {
+ int a=0;
+ int h=0;
+ printf("wpisz podstawe ");
+ scanf("%d" , &a); 
+ printf("wpisz wysokosc ");
+ scanf("%d" , &h);
+ printf("Pole trojkata = %d",a*h/2);
+	
+ return 0;
+ }
+ 
